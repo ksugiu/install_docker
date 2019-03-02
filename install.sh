c5facd8f34dc879311096d0c6a4845aeee0cd98e
ksugiu <31092377+ksugiu@users.noreply.github.com>
2019-03-02 17:34:27 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo apt-get install -yqq apt-transport-https ca-certificates curl gnupg-agent s
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install -yqq docker-ce docker-ce-cli containerd.io
+sudo apt-get install -yqq docker-ce=18.06.2~ce~3-0~ubuntu
