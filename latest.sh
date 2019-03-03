41b4d4628fd1e62a9465955623ef2936a7f5eb2e
ksugiu <31092377+ksugiu@users.noreply.github.com>
2019-03-03 12:37:35 +0100

    Create latest.sh

---
+++ latest.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -yqq apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install -yqq docker-ce
